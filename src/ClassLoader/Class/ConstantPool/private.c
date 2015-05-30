ed75488cf33e9862707080555e3fe6cf0e7c4cac
Abílio Oliveira <eco.abilio@gmail.com>
2015-05-29 23:05:24 -0300

    Reorganizacao

--- src/ClassLoader/Class/ConstantPool/private.c
+++ src/ClassLoader/Class/ConstantPool/private.c
@@ -1,42 +1,5 @@
 #include "constantpool.h"
 
-// declaracoes
-static void populateUtf8(struct _constant_info*, int ordem, DADOS d, int* contador);
-static void populateMethodRef(struct _constant_info* constants, int ordem, DADOS d, int* contador);
-
-static void addConstant(CONSTANT_POOL* this, int ordem, DADOS d, int* contador) {
-	this->constants[ordem].tag = d.bytes[(*contador)++];
-	switch(this->constants[ordem].tag) {
-		case tUtf8:
-			populateUtf8(this->constants, ordem, d, contador);
-			break;
-		case tInteger:
-			break;
-		case tFloat:
-			break;
-		case tLong:
-			break;
-		case tDouble:
-			break;
-		case tClass:
-			break;		
-		case tString:
-			break;		
-		case tFieldRef:
-
-			break;		
-		case tMethodRef:
-			populateMethodRef(this->constants, ordem, d, contador);
-			break;		
-		case tInterfaceMethodRef:
-			break;		
-		case tNameType:
-			break;
-		default:
-			break;	
-	}
-}
-
 static void populateUtf8(struct _constant_info* constants, int ordem, DADOS d, int* contador) {
 	int i = 0;
 	uint16_t tam = d.bytes[*contador++];
--- src/ClassLoader/Class/ConstantPool/public.c
+++ src/ClassLoader/Class/ConstantPool/public.c
@@ -1,12 +1,43 @@
 #include "constantpool.h"
 #include "private.c"
 
+static void addConstant(CONSTANT_POOL* this, int ordem, DADOS d, int* contador) {
+	this->constants[ordem].tag = d.bytes[(*contador)++];
+	switch(this->constants[ordem].tag) {
+		case tUtf8:
+			populateUtf8(this->constants, ordem, d, contador);
+			break;
+		case tInteger:
+			break;
+		case tFloat:
+			break;
+		case tLong:
+			break;
+		case tDouble:
+			break;
+		case tClass:
+			break;		
+		case tString:
+			break;		
+		case tFieldRef:
+			break;		
+		case tMethodRef:
+			populateMethodRef(this->constants, ordem, d, contador);
+			break;		
+		case tInterfaceMethodRef:
+			break;		
+		case tNameType:
+			break;
+		default:
+			break;	
+	}
+}
 
 CONSTANT_POOL* initCONSTANT_POOL(int count) {
 	CONSTANT_POOL* toReturn = (CONSTANT_POOL*)malloc(sizeof(CONSTANT_POOL));
 
 	toReturn->constants = (struct _constant_info*)malloc(count*sizeof(struct _constant_info));
 	toReturn->addConstant = addConstant;
-	
+
 	return toReturn;
 }
