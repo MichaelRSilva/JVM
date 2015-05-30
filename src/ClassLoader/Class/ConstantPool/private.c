a3b71e1fcbcc8af897f2acf41ac3a3c721f23e81
Michael Rodrigues <michaeldf.ti@gmail.com>
2015-05-29 23:57:05 -0300

    De cima para baixo, de NameType para ClassRef funções implementadas em constantPool

--- src/ClassLoader/Class/ConstantPool/private.c
+++ src/ClassLoader/Class/ConstantPool/private.c
@@ -25,3 +25,54 @@ static void populateMethodRef(struct _constant_info* constants, int ordem, DADOS
 	constants[ordem].type.MethodRef.nameTypeIndex = nameTypeIndex;
 }
 
+
+static void populateNameType(struct _constant_info* constants, int ordem, DADOS d, int* contador) {
+	uint16_t nameIndex = d.bytes[(*contador)++], descriptorIndex = 0;
+
+	nameIndex =  nameIndex << 8 | d.bytes[(*contador)++];
+	descriptorIndex = d.bytes[(*contador)++];
+	descriptorIndex = descriptorIndex << 8  | d.bytes[(*contador)++];
+
+	constants[ordem].type.NameType.nameIndex = nameIndex;
+	constants[ordem].type.NameType.descriptorIndex = descriptorIndex;
+}
+
+static void populateInterfaceMehodRef(struct _constant_info* constants, int ordem, DADOS d, int* contador) {
+	uint16_t classIndex = d.bytes[(*contador)++], nameTypeIndex = 0;
+
+	classIndex =  classIndex << 8 | d.bytes[(*contador)++];
+	nameTypeIndex = d.bytes[(*contador)++];
+	nameTypeIndex = nameTypeIndex << 8  | d.bytes[(*contador)++];
+
+	constants[ordem].type.InterfaceMethodRef.classIndex = classIndex;
+	constants[ordem].type.InterfaceMethodRef.nameTypeIndex = nameTypeIndex;
+}
+
+static void populateFieldRef(struct _constant_info* constants, int ordem, DADOS d, int* contador) {
+	uint16_t classIndex = d.bytes[(*contador)++], nameTypeIndex = 0;
+
+	classIndex =  classIndex << 8 | d.bytes[(*contador)++];
+	nameTypeIndex = d.bytes[(*contador)++];
+	nameTypeIndex = nameTypeIndex << 8  | d.bytes[(*contador)++];
+
+	constants[ordem].type.FieldRef.classIndex = classIndex;
+	constants[ordem].type.FieldRef.nameTypeIndex = nameTypeIndex;
+}
+
+static void populateStringRef(struct _constant_info* constants, int ordem, DADOS d, int* contador) {
+	uint16_t stringIndex = d.bytes[(*contador)++];
+
+	stringIndex =  stringIndex << 8 | d.bytes[(*contador)++];
+
+	constants[ordem].type.String.stringIndex = stringIndex;
+}
+
+static void populateClassRef(struct _constant_info* constants, int ordem, DADOS d, int* contador) {
+	uint16_t nameIndex = d.bytes[(*contador)++];
+
+	nameIndex =  nameIndex << 8 | d.bytes[(*contador)++];
+
+	constants[ordem].type.Class.nameIndex = nameIndex;
+}
+
+
