0a1137895a041ada6a92935fed3662e3cad52ab1
Abílio Oliveira <eco.abilio@gmail.com>
2015-05-31 18:44:58 -0300

    implementado populateConstantValueAttribute e populateCodeAttribute

--- src/ClassLoader/Class/AttributeInfo/attributeinfo.h
+++ src/ClassLoader/Class/AttributeInfo/attributeinfo.h
@@ -11,6 +11,34 @@
 #ifndef MODULE_ATTRIBUTE_INFO
 	#define MODULE_ATTRIBUTE_INFO
 	#include "../../../Common/common.h"
+	#include "../ConstantPool/constantpool.h"
+
+	struct _exception_table {
+		uint16_t start_pc;
+		uint16_t end_pc;
+		uint16_t handler_pc;
+		uint16_t catch_type;
+	};
+
+	struct _classes {
+		uint16_t inner_class_info_index;
+		uint16_t outer_class_info_index;
+		uint16_t inner_name_index;
+		uint16_t inner_class_access_flags;
+	};
+
+	struct _local_variable_table {
+		uint16_t start_pc;
+		uint16_t length;
+		uint16_t name_index;
+		uint16_t descriptor_index;
+		uint16_t index;
+	};
+
+	struct _line_number_table {
+		uint16_t start_pc;
+		uint16_t line_number;
+	};
 	
 	struct _attribute_info {
 		uint16_t attributeNameIndex;
@@ -19,54 +47,42 @@
 			struct {
 				uint16_t constantvalue_index;
 			} ConstantValueAttribute;
+
 			struct {
 				uint16_t max_stack;
 				uint16_t max_locals;
 				uint32_t code_length;
 				uint8_t* code;
 				uint16_t exception_table_length;
-				struct {
-					uint16_t start_pc;
-					uint16_t end_pc;
-					uint16_t handler_pc;
-					uint16_t catch_type;
-				} *exception_table;
+				struct _exception_table *exception_table;
 				uint16_t attributes_count;
 				struct _attribute_info* attributes; 
 			} CodeAttribute;
+
 			struct {
 				uint16_t number_of_exceptions;
 				uint16_t* exception_index_table;
 			} ExeceptionsAttribute;
+
 			struct {
 				uint16_t number_of_classes;
-				struct {
-					uint16_t inner_class_info_index;
-					uint16_t outer_class_info_index;
-					uint16_t inner_name_index;
-					uint16_t inner_class_access_flags;
-				} *classes;
+				struct _classes *classes;
 			} InnerClassesAttribute;
+
 			struct {} SyntheticAttribute;
+
 			struct {
 				uint16_t sourcefile_index;
 			} SourceFileAttribute;
+
 			struct {
 				uint16_t liner_number_table_length;
-				struct {
-					uint16_t start_pc;
-					uint16_t line_number;
-				} *line_number_table;
+				struct _line_number_table *line_number_table;
 			} LineNumberTableAttribute;
+
 			struct {
 				uint16_t local_variable_table_length;
-				struct {
-					uint16_t start_pc;
-					uint16_t length;
-					uint16_t name_index;
-					uint16_t descriptor_index;
-					uint16_t index;
-				} *local_variable_table;
+				struct _local_variable_table *local_variable_table;
 			} LocalVariableTableAttribute;
 			struct {} Deprecated;
 		} info;
@@ -74,7 +90,7 @@
 
 	typedef struct _attributes {
 		struct _attribute_info* attributes;
-		void (*addAttribute)(struct _attributes*, int, DADOS*);
+		void (*addAttribute)(struct _attributes*, CONSTANT_POOL*, int, DADOS*);
 	} ATTRIBUTE_POOL;
 
 	ATTRIBUTE_POOL* initATTRIBUTE_POOL(int);
