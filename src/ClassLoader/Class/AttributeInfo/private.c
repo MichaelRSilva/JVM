9e986d161d51991bd1f31b2ab4337a38ffcd71ec
Michael Rodrigues <michaeldf.ti@gmail.com>
2015-05-31 23:23:05 -0300

    populates do attribute finalizados. os que não usam estão com //NOTHING

--- src/ClassLoader/Class/AttributeInfo/private.c
+++ src/ClassLoader/Class/AttributeInfo/private.c
@@ -39,29 +39,61 @@ static void populateCodeAttribute(struct _attribute_info* a, CONSTANT_POOL* cp, 
 }
 
 static void populateExceptions(struct _attribute_info* a, DADOS* d) {
+	a->info.ExeceptionsAttribute.number_of_exceptions = d->le2Bytes(d);
+	a->info.ExeceptionsAttribute.exception_index_table = malloc (a->info.ExeceptionsAttribute.number_of_exceptions * sizeof(struct _exception_table));
 
+	for(int i=0; i < a->info.ExeceptionsAttribute.number_of_exceptions ; i++){
+		a->info.ExeceptionsAttribute.exception_index_table[i] = d->le2Bytes(d);
+	}
 }
 
 static void populateInnerClasses(struct _attribute_info* a, DADOS* d) {
+	a->info.InnerClassesAttribute.number_of_classes = d->le2Bytes(d);
+	a->info.InnerClassesAttribute.classes = malloc (a->info.InnerClassesAttribute.number_of_classes * sizeof(struct _classes));
 
+	for(int i=0; i < a->info.InnerClassesAttribute.number_of_classes ; i++){
+		a->info.InnerClassesAttribute.classes[i].inner_class_info_index = d->le2Bytes(d);
+		a->info.InnerClassesAttribute.classes[i].outer_class_info_index = d->le2Bytes(d);
+		a->info.InnerClassesAttribute.classes[i].inner_name_index = d->le2Bytes(d);
+		a->info.InnerClassesAttribute.classes[i].inner_class_access_flags = d->le2Bytes(d);
+	}
 }
 
 static void populateSynthetic(struct _attribute_info* a, DADOS* d) {
-
+	//NOTHING
 }
 
 static void populateSourceFile(struct _attribute_info* a, DADOS* d) {
-
+	a->info.SourceFileAttribute.sourcefile_index = d->le2Bytes(d);
 }
 
 static void populateLineNumberTable(struct _attribute_info* a, DADOS* d) {
+	a->info.LineNumberTableAttribute.liner_number_table_length = d->le2Bytes(d);
+	a->info.LineNumberTableAttribute.line_number_table = malloc (a->info.LineNumberTableAttribute.liner_number_table_length * sizeof(struct _line_number_table));
 
+	for(int i=0; i < a->info.LineNumberTableAttribute.liner_number_table_length ; i++){
+		a->info.LineNumberTableAttribute.line_number_table[i].start_pc = d->le2Bytes(d);
+		a->info.LineNumberTableAttribute.line_number_table[i].line_number = d->le2Bytes(d);
+	}
 }
 
 static void populateLocalVariableTable(struct _attribute_info* a, DADOS* d) {
+	
+	a->info.LocalVariableTableAttribute.local_variable_table_length = d->le2Bytes(d);
+	a->info.LocalVariableTableAttribute.local_variable_table = malloc (a->info.LocalVariableTableAttribute.local_variable_table_length * sizeof(struct _local_variable_table));
+
+	for(int i=0; i < a->info.InnerClassesAttribute.number_of_classes ; i++){
+		a->info.LocalVariableTableAttribute.local_variable_table[i].start_pc = d->le2Bytes(d);
+		a->info.LocalVariableTableAttribute.local_variable_table[i].length = d->le2Bytes(d);
+		a->info.LocalVariableTableAttribute.local_variable_table[i].name_index = d->le2Bytes(d);
+		a->info.LocalVariableTableAttribute.local_variable_table[i].descriptor_index = d->le2Bytes(d);
+		a->info.LocalVariableTableAttribute.local_variable_table[i].index = d->le2Bytes(d);
+	}
+}
+
 
 }
 
 static void populateDeprecated(struct _attribute_info* a, DADOS* d) {
-
+	//NOTHING
 }
