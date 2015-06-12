eb631c72e919dd7c5ae6637cc7a8e798e1c35ee8
Abílio Oliveira <eco.abilio@gmail.com>
2015-06-11 21:22:17 -0300

    incorporado main ao public.c

--- src/JVM/public.c
+++ src/JVM/public.c
@@ -1,3 +1,13 @@
+/*! \file
+	\brief Main entry point da Máquina Virtual
+
+	Autores:
+		- Abilio Esteves Calegario de Oliveira - 10/0006132
+		- Marcus da Silva Ferreira - 10/0056881
+		- Michael Rodrigues - 09/0126432
+
+	JVM - Software Basico 1/2015
+*/
 #include "jvm.h"
 #include "private.c"
 
@@ -28,4 +38,23 @@ JVM initJVM() {
 	toReturn.loadParentClasses = loadParentClasses;
 	return toReturn;
 }
+
+int main(int argc, char **argv) {
+	int flag;
+	UTIL util = getUTILInstance();
+	CLASS_LOADER* cl = initCLASS_LOADER();
+
+	if(!(flag = util.VerificaLeitorExibidorCMDArgs(argc, argv))) {
+		if (!(flag = cl->load(cl, util.LeArquivo(argv[1])))) {
+			JVM maquina = initJVM();
+			maquina.classes[maquina.class_array_size++] = cl->class;
+			flag = maquina.loadParentClasses(&maquina, cl->class, &util);
+
+		}
+	}
+	
+	printf("%s", errordesc[abs(flag)].message);
+	return 0;
+}
+
 
