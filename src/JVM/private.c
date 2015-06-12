5556ca6bbcc51ebb43f92dfa2c7e6d122134d27f
Michael Rodrigues <michaeldf.ti@gmail.com>
2015-06-11 23:40:29 -0300

    no message

--- src/JVM/private.c
+++ src/JVM/private.c
@@ -1,17 +1,25 @@
 #include "jvm.h"
 
-int classIndex(char* name, CLASS** classes) {
-	// TODO
-	return 0;
+int classIndex(char* name, JVM* maquina) {
+
+	for(int i=0; i < (maquina->class_array_size); i++){
+		char *aux = (char*)maquina->classes[i]->constant_pool->constants[maquina->classes[i]->constant_pool->constants[maquina->classes[i]->this_class -1].type.Class.nameIndex-1].type.Utf8.bytes;
+		if(strcmp(name,aux)){
+			return i;
+		}
+	}
+	return -1;
 }
+
 
 char* getParentName(CLASS* class) {
 	return (char*)class->constant_pool->constants[class->constant_pool->constants[class->super_class - 1].type.Class.nameIndex - 1].type.Utf8.bytes;
 }
 
-char* getParentPath(CLASS* class, char* parentName) {
-	char* path = (char*)malloc(strlen(parentName) + 7);
-	strcpy(path, parentName);
+char* getPath(char* name) {
+	char* path = (char*)malloc(strlen(name) + 7);
+	path[strlen(name)+6] = '\0';
+	strcpy(path, name);
 	strcat(path, ".class");
 	return path;
 }
--- src/JVM/public.c
+++ src/JVM/public.c
@@ -16,11 +16,11 @@ static int loadParentClasses(JVM* maquina, CLASS* class, UTIL* util) {
 	CLASS_LOADER *cl = initCLASS_LOADER();
 	int flag = 0;
 
-	if (classIndex(parentName, maquina->classes) > -1) {
+	if (classIndex(parentName, maquina) == -1) {
 		// adiciona classe pai a maquina->classes
 		maquina->classes = (CLASS**)realloc(maquina->classes,(maquina->class_array_size+1)*sizeof(CLASS*));
 
-		cl->load(cl, util->LeArquivo(getParentPath(class, parentName)));
+		cl->load(cl, util->LeArquivo(getPath(parentName)));
 		maquina->classes[maquina->class_array_size++] = cl->class;
 		if (maquina->classes[maquina->class_array_size-1]->super_class != 0) {
 			flag = loadParentClasses(maquina, maquina->classes[maquina->class_array_size], util);
@@ -28,6 +28,24 @@ static int loadParentClasses(JVM* maquina, CLASS* class, UTIL* util) {
 	}
 
 	return flag;
+}
+
+static int loadInterfaces(JVM* maquina, CLASS* class, UTIL* util) {
+	int interfacesCount = class->interfaces_count;
+	CLASS_LOADER *cl = initCLASS_LOADER();
+
+	for(int i=0; i<interfacesCount; i++){
+		maquina->classes = (CLASS**)realloc(maquina->classes,(maquina->class_array_size+1)*sizeof(CLASS*));
+		char* name = (char*)class->constant_pool->constants[class->constant_pool->constants[class->interfaces[i] -1].type.Class.nameIndex-1].type.Utf8.bytes;
+		
+		if (classIndex(name, maquina) == -1) {
+			cl->load(cl, util->LeArquivo(getPath(name)));
+			maquina->classes[maquina->class_array_size++] = cl->class;
+		}
+		
+	}
+
+	return E_SUCCESS;
 }
 
 JVM initJVM() {
@@ -50,8 +68,11 @@ int main(int argc, char **argv) {
 		if (!(flag = cl->load(cl, util.LeArquivo(argv[1])))) {
 			JVM maquina = initJVM();
 			maquina.classes[maquina.class_array_size++] = cl->class;
-			flag = maquina.loadParentClasses(&maquina, cl->class, &util);
-
+			flag = loadParentClasses(&maquina, cl->class, &util);
+			if(!flag){
+				flag = loadInterfaces(&maquina, cl->class, &util);
+			}
+			
 		}
 	}
 	
